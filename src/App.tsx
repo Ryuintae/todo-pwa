23cd91019aaafd81ee298c3f6248fe96814141c5
Ryuintae <dlsxochlrg@ejbt.co.kr>
2026-01-27 00:10:32 +0900

    gitignore 수정, App.tsx로 변경

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './index.css'
+
+function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-neutral-900">
+            <div className="text-2xl font-bold text-white">TodoList</div>
+        </div>
+    )
+}
+
+export default App
