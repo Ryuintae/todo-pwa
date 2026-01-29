774b767b632acc3cd70fb35745df37bbeb6746a5
RyuIntae <dlsxochlrg@ejbt.co.kr>
2026-01-29 21:15:41 +0900

    초기 Todos 생성 로직 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,51 @@
 import './App.css'
 import './index.css'
+import {useState} from "react";
+
+interface Todo {
+    text: string
+}
 
 function App() {
+    const [task, setTask] = useState<string>("")
+    const [todos, setTodos] = useState<Todo[]>([])
     return (
-        <div className="min-h-screen flex items-center justify-center bg-neutral-900">
-            <div className="text-2xl font-bold text-white">TodoList</div>
-        </div>
+        <>
+            <div className="min-h-screen flex items-center justify-center bg-neutral-600">
+                <div className="text-2xl font-bold text-white">TodoList</div>
+
+                <input className={"px-3 py-2 text-black rounded"}
+                       value={task}
+                       onChange={(event) => {
+                           setTask(event.target.value)
+                       }}
+                />
+
+                <button className={"px-y py-2 bg-blue-600 rounded"}
+                        onClick={() => {
+                            if (task.trim() === "") return;
+                            setTodos(prev => [...prev, {text: task}]);
+                            setTask("");
+                        }}
+                >
+                    할 일 추가
+                </button>
+
+                <div className={"mt-4 w-64 flex flex-col gap-2"}>
+                    {todos.map((todo, index) =>
+                        <div
+                            key={index}
+                            className={"bg-neutral-400 px-3 py-2 rounded"}
+                        >
+                            {todo.text}
+                        </div>
+                    )}
+
+                </div>
+
+            </div>
+
+        </>
     )
 }
 
